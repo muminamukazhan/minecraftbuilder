b0ccc97bd5ddbc1fa3a55e451a0efe78507c2265
muminamukazhan <mukazhan13@gmail.com>
2026-09-13 19:57:20 +0500

    the structure like dessert, mineshaft

---
+++ src/com/minecraft/builder/product/Structure.java
@@ -0,0 +1,11 @@
+package com.minecraft.builder.product;
+
+public enum Structure {
+    VILLAGE,
+    STRONGHOLD,
+    MINESHAFT,
+    DESERT_TEMPLE,
+    OCEAN_MONUMENT,
+    NETHER_FORTRESS,
+    SUPERFLAT_PLATFORM
+}
